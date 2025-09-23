7f1e1a9be70c22711fd7af9a2d1965e01692123f
Ashwini H K <79743464+ASHWINI092k@users.noreply.github.com>
2025-09-23 19:50:42 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        int num1 = 10;
-        int num2 = 20;
+        int num1 = 178;
+        int num2 = 298;
         int sum = num1 + num2;
 
         System.out.println("Sum: " + sum);
